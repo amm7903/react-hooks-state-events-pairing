189cbc064f0449a95ae0bb0fb20059cb253fa1a9
Alejandro Marcano <+>
2021-10-14 23:34:45 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import video from "../data/video.js";
+import Comments from "./Comments.js";
 
 function App() {
   console.log("Here's your data:", video);
@@ -13,6 +14,7 @@ function App() {
         allowFullScreen
         title="Thinking in React"
       />
+    <Comments comments={video}/>
     </div>
   );
 }
---
+++ src/components/Comments.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function Comments({comments}) {
+    return (
+        <div>
+                                               
+        
+            <h2>    {comments.length} Comments </h2>
+            
+            
+        </div>
+    )
+}
+
+export default Comments
